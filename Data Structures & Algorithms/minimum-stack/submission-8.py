3dd5b71c93c8b0a8ec9847c038b7d5161875189b
thalleous <137974653+Thalleous88@users.noreply.github.com>
2026-07-19 20:35:30 +0700

    Add: minimum-stack - submission-8

---
+++ Data Structures & Algorithms/minimum-stack/submission-8.py
@@ -0,0 +1,32 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minstack = []
+        self.queue = deque()
+
+    
+        
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        
+        if not self.minstack or self.minstack[-1] >= val:
+            self.minstack.append(val)
+
+
+    def pop(self) -> None:
+        num = self.stack.pop()
+
+        if self.minstack and self.minstack[-1] == num:
+            self.minstack.pop()
+
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+        
+    def getMin(self) -> int:
+        return self.minstack[-1]
+
+        
