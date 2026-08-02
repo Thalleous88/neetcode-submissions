6de34e045889c7bbd15ac68cdd57a332cedd04c1
thalleous <137974653+Thalleous88@users.noreply.github.com>
2026-08-02 11:18:47 +0700

    Add: jump-game - submission-13

---
+++ Data Structures & Algorithms/jump-game/submission-13.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        
+        index = 0
+        memo = {}
+
+        def dp(index):
+            if index >= len(nums)-1:
+                return True
+
+            if index in memo:
+                return memo[index]
+
+            temp = nums[index]
+            while temp > 0:
+                if dp(index + temp):
+                    memo[index] = True
+                    return True
+                    
+                temp -= 1
+
+            memo[index] = False
+            return memo[index]
+
+
+        return dp(0)
