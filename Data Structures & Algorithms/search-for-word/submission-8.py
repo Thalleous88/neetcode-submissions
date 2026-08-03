9b1288e3a99adc8369952bd032fce845f5821d68
thalleous <137974653+Thalleous88@users.noreply.github.com>
2026-08-03 12:06:38 +0700

    Add: search-for-word - submission-8

---
+++ Data Structures & Algorithms/search-for-word/submission-8.py
@@ -0,0 +1,40 @@
+import copy
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        word = list(word)
+        row, col = len(board), len(board[0])
+
+        def dfs(x, y, windex, word, new):
+            if x < 0 or x >= row or y < 0 or y >= col or new[x][y] == -1 or windex >= len(word) or word[windex] != new[x][y]:
+                return False
+
+            if windex == len(word) - 1 and word[windex] == new[x][y]:
+                return True
+
+
+            if word[windex] == new[x][y]:
+                temp = new[x][y]
+                new[x][y] = -1
+                find = dfs(x+1, y, windex+1, word, new) or dfs(x, y+1, windex+1, word, new) or dfs(x-1, y, windex+1, word, new) or dfs(x, y-1, windex+1, word, new)
+
+                new[x][y] = temp
+                return find
+
+
+        
+
+        for i in range(row):
+            for j in range(col):
+                if board[i][j] == word[0]:
+                    new = copy.deepcopy(board)
+                    if len(word) == 1:
+                        return True
+                    
+                    if dfs(i, j, 0, word, new):
+                        return True
+
+        return False
+
+        
+
